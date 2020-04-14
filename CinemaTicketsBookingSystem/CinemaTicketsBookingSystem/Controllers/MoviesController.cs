66509dbedf8cf8423115df6ddba6bba8381122b9
Tijana <tijana.ilic87@protonmail.com>
2020-04-14 21:04:40 +0200

    Create Movies controller and Index view
    Adjust css

---
+++ CinemaTicketsBookingSystem/CinemaTicketsBookingSystem/Controllers/MoviesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CinemaTicketsBookingSystem.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CinemaTicketsBookingSystem.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public MoviesController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var movies = _db.Movies.Include(m => m.Genre);
+            return View(await movies.ToListAsync());
+        }
+    }
+}
