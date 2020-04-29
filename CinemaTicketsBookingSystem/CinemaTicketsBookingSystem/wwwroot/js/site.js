53c91f1c672df0eee0902746dc2f6a78e002bdba
Tijana <tijana.ilic87@protonmail.com>
2020-04-29 09:51:26 +0200

    Fix responsivness of ShoppingCarts/Index and Showtimes/Buy

--- CinemaTicketsBookingSystem/CinemaTicketsBookingSystem/wwwroot/js/site.js
+++ CinemaTicketsBookingSystem/CinemaTicketsBookingSystem/wwwroot/js/site.js
@@ -2,3 +2,23 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(window).on('resize', mobileSize);
+$(window).on('load', mobileSize);
+
+function mobileSize() {
+    if ($(window).width() < 767) {
+        $('.col-resp').removeClass('col-8');
+        $('.col-resp').removeClass('col-lg-8');
+        $('.input-resp').removeClass('col-2');
+        $('.input-resp').addClass('col-6');
+        $('.h-resp').hide();
+    }
+    else {
+        $('.col-resp').addClass('col-8');
+        $('.col-resp').addClass('col-lg-8');
+        $('.input-resp').addClass('col-2');
+        $('.input-resp').removeClass('col-6');
+        $('.h-resp').show();
+    }
+}
